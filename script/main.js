069149a4dc0ac52344830c2f3ef78a1d5183faea
zuzexx <tjasa.zilavec@gmail.com>
2023-03-17 20:36:36 +0100

    .background done

--- script/main.js
+++ script/main.js
@@ -15,56 +15,57 @@ forrestLayer4.src = "../assets/PNG/Day/Forest/Forest Layer 04.png";
 const forrestLayer5 = new Image();
 forrestLayer5.src = "../assets/PNG/Day/Forest/Forest Layer 05.png";
 
-class Layer {
-  constructor(image, speedModifier) {
-    this.x = 0;
-    this.y = 0;
-    this.width = CANVAS_WIDTH;
-    this.height = CANVAS_HEIGHT;
-    this.x2 = this.width;
-    this.image = image;
-    this.speedModifier = speedModifier;
-    this.speed = gameSpeed * this.speedModifier;
-  }
-  update() {
-    this.speed = gameSpeed * this.speedModifier;
-    if (this.x <= -this.width) {
-      this.x = this.width + this.x2 - this.speed;
+window.addEventListener("load", function () {
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = CANVAS_WIDTH;
+      this.height = CANVAS_HEIGHT;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
     }
-    if (this.x2 <= -this.width) {
-      this.x2 = this.width + this.x - this.speed;
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      if (this.x <= -this.width) {
+        this.x = 0;
+      }
+      this.x = this.x - this.speed;
+      //this.x = (gameFrame * this.speed) % this.width;
     }
-    this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.x + this.width,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
   }
-  draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
-  }
-}
 
-const layer1 = new Layer(forrestLayer1, 0.5);
-const layer2 = new Layer(forrestLayer2, 0.6);
-const layer3 = new Layer(forrestLayer3, 0.75);
-const layer4 = new Layer(forrestLayer4, 0.8);
-const layer5 = new Layer(forrestLayer5, 1);
+  const layer1 = new Layer(forrestLayer1, 0.5);
+  const layer2 = new Layer(forrestLayer2, 0.6);
+  const layer3 = new Layer(forrestLayer3, 0.75);
+  const layer4 = new Layer(forrestLayer4, 0.8);
+  const layer5 = new Layer(forrestLayer5, 1);
 
-const drawAndUpdate = () => {
-  layer1.update();
-  layer1.draw();
-  layer2.update();
-  layer2.draw();
-  layer3.update();
-  layer3.draw();
-  layer4.update();
-  layer4.draw();
-  layer5.update();
-  layer5.draw();
-};
+  const forrestLayers = [layer1, layer2, layer3, layer4, layer5];
 
-const backgroundAnimate = () => {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  drawAndUpdate();
-  requestAnimationFrame(backgroundAnimate);
-};
-backgroundAnimate();
+  const drawAndUpdate = () => {
+    forrestLayers.forEach((layer) => {
+      layer.update();
+      layer.draw();
+    });
+  };
+
+  const backgroundAnimate = () => {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    drawAndUpdate();
+
+    requestAnimationFrame(backgroundAnimate);
+  };
+  backgroundAnimate();
+});
