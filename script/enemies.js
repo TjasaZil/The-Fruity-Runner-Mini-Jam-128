2c3cd67d0a54bacf69b631cf5fda03b3509371b9
zuzexx <tjasa.zilavec@gmail.com>
2023-03-18 17:44:26 +0100

    randomized enemies some more

--- script/enemies.js
+++ script/enemies.js
@@ -5,10 +5,11 @@ class Enemy {
     this.fps = 5;
     this.frameInterval = 1000 / this.fps;
     this.frameTimer = 0;
+    this.markedForDeletion = false;
   }
   update(deltaTime) {
     // movement of the trap
-    this.x -= this.speedX;
+    this.x -= this.speedX + this.game.speed;
     this.y += this.speedY;
     if (this.frameTimer > this.frameInterval) {
       this.frameTimer = 0;
@@ -17,6 +18,9 @@ class Enemy {
     } else {
       this.frameTimer += deltaTime;
     }
+    //check if off screen
+
+    if (this.x + this.width < 0) this.markedForDeletion = true;
   }
   draw(context) {
     context.drawImage(
@@ -38,7 +42,7 @@ class Bush1 extends Enemy {
     this.game = game;
     this.width = 50;
     this.height = 50;
-    this.x = 480;
+    this.x = this.game.width + Math.random() * this.game.width * 0.5;
     this.y = this.game.height - this.height - this.game.groundMargin;
     this.speedX = 2;
     this.speedY = 0;
@@ -55,7 +59,7 @@ class Bush2 extends Enemy {
     this.game = game;
     this.width = 50;
     this.height = 34;
-    this.x = 480;
+    this.x = this.game.width + Math.random() * this.game.width * 0.5;
     this.y = this.game.height - this.height - this.game.groundMargin;
     this.speedX = 2;
     this.speedY = 0;
@@ -69,7 +73,7 @@ class Trap extends Enemy {
     this.game = game;
     this.width = 50;
     this.height = 12;
-    this.x = 480;
+    this.x = this.game.width + Math.random() * this.game.width * 0.5;
     this.y = this.game.height - this.height - this.game.groundMargin;
     this.speedX = 2;
     this.speedY = 0;
